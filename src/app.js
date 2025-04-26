731a0f648fe6fbc484b18dfb71f920f4d8541373
Zahin Alwa <zahin.alwa@gmail.com>
2025-04-26 23:43:51 +0530

    [Feature] (Video play/pause and app back btn)

--- src/app.js
+++ src/app.js
@@ -2,15 +2,22 @@
   /** Set Global variables and cache DOM element refs **/
   let selectedColor = "";
   let hasGameStarted = false;
+  let isBgPlayStarted = false;
   const vsComputerEl = document.getElementById("vs-computer");
   const colorSelectionList = document.querySelectorAll(".js-color-selection");
   const csContainerEl = document.querySelector(".js-cs-wrapper");
   const heroBtnContainerEl = document.querySelector(".js-hero-buttons");
   const ctaButtonContainerEl = document.querySelector(".js-cta-buttons");
   const backBtnEl = document.querySelector(".js-back-btn");
+  const appBackBtnEl = document.querySelector(".js-app-back-btn");
   const playBtnEl = document.querySelector(".js-play-btn");
   const splashScreenEl = document.querySelector(".js-splashScreen");
   const appEl = document.querySelector(".js-app");
+  const backgroundPlayEl = document.querySelector(".js-icon-bg-play");
+  const bgPlayIconPath = new URL("assets/icon-video-play.png", import.meta.url);
+  const bgPauseIconPath = new URL("assets/icon-video-pause.png", import.meta.url);
+  const gameMusicPath = new URL('assets/game-music.mp3', import.meta.url);
+  const gameAudio = new Audio(gameMusicPath);
 
   function handleSelectionClick(e) {
     const el = e.currentTarget;
@@ -31,7 +38,6 @@
       selectionEl.dataset["flag"] = "0";
     });
   }
-
   function handleVSComputerClick() {
     heroBtnContainerEl.classList.add("d-none");
     csContainerEl.classList.remove("d-none");
@@ -41,7 +47,6 @@
     // Reset all color selection nodes
     colorSelectionList.forEach((selectionEl) => selectionEl.dataset["flag"] = "0");
   }
-
   function handleBackBtnClick() {
     playBtnEl.disabled = true;
     selectedColor = "";
@@ -51,7 +56,6 @@
     csContainerEl.classList.add("d-none");
     ctaButtonContainerEl.classList.add("d-none");
   }
-
   function handlePlayBtnClick() {
     if (!selectedColor) { return false; }
 
@@ -59,12 +63,59 @@
     appEl.classList.remove("d-none");
     hasGameStarted = true;
   }
+  function handleAppBackBtnClick() {
+    const isAppLogout = window.confirm("Do you wish to exit this game?");
 
+    if (!isAppLogout) {
+      return false;
+    }
+
+    hasGameStarted = false;
+    appEl.classList.add("d-none");
+    splashScreenEl.classList.remove("d-none");
+    // Reset user onboarding
+    csContainerEl.classList.add("d-none");
+    ctaButtonContainerEl.classList.add("d-none");
+    heroBtnContainerEl.classList.remove("d-none");
+    playBtnEl.disabled = true;
+    selectedColor = "";
+    // Reset all color selection nodes
+    colorSelectionList.forEach((selectionEl) => selectionEl.dataset["flag"] = "0");
+  }
+  function handleBgPlayClick(e) {
+    const el = e.currentTarget;
+    const img = el.querySelector(".icon-bg-play");
+
+    isBgPlayStarted = !isBgPlayStarted;
+    if (!isBgPlayStarted) {
+      img.src = bgPlayIconPath;
+      pauseAudio(gameAudio);
+      return;
+    }
+
+    img.src = bgPauseIconPath;
+    playAudio(gameAudio, true);
+    console.log("background play click:");
+  }
+  function pauseAudio(audioRef) {
+    audioRef.pause();
+  }
+  function playAudio(audioRef, isLoop = false) {
+    audioRef.loop = isLoop;
+
+    audioRef
+    .play()
+    .catch((err) => {
+      console.log("Audio is not played ", err);
+    });
+  }
   function setDOMEvents() {
     colorSelectionList.forEach((el) => el.addEventListener("click", handleSelectionClick, false));
     vsComputerEl.addEventListener("click", handleVSComputerClick, false);
     backBtnEl.addEventListener("click", handleBackBtnClick, false);
+    appBackBtnEl.addEventListener("click", handleAppBackBtnClick, false);
     playBtnEl.addEventListener("click", handlePlayBtnClick, false);
+    backgroundPlayEl.addEventListener("click", handleBgPlayClick, false);
   }
 
   console.log("Ludo game JS loaded.");
