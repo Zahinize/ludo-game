dfc7b30941ff40f49d62023276697c19feb88c85
Zahin Alwa <zahin.alwa@gmail.com>
2025-06-17 00:43:58 +0530

    AppJS: Setup layout based on user color selection

--- src/app.js
+++ src/app.js
@@ -9,6 +9,12 @@
   const GREEN_COLOR = "green";
   const YELLOW_COLOR = "yellow";
   const colorSequence = [BLUE_COLOR, RED_COLOR, GREEN_COLOR, YELLOW_COLOR];
+  const colorMap = {
+    [RED_COLOR]: "#EA1D23",
+    [GREEN_COLOR]: "#00A347",
+    [BLUE_COLOR]: "#29ADFF",
+    [YELLOW_COLOR]: "#DABC0F"
+  };
   const vsComputerEl = document.getElementById("vs-computer");
   const colorSelectionList = document.querySelectorAll(".js-color-selection");
   const csContainerEl = document.querySelector(".js-cs-wrapper");
@@ -27,7 +33,7 @@
   const gameAudio = new Audio(gameMusicPath);
   const interactionAudio = new Audio(interactionMusicPath);
 
-  function getRivalColors(color) {
+  function getActiveColors(color) {
     if (color == "") return null;
     color = color.toLowerCase();
 
@@ -153,15 +159,80 @@
       document.querySelector(`.js-token-${color}-4`).classList.add("d-none");
     });
   }
+  function updateGameTokenStyles(selectedColor = "", computerColor = "") {
+    // Update game token positions for computer player
+    document.querySelector(`.js-token.js-token-${computerColor}-1`).classList.add("game-token-computer-1");
+    document.querySelector(`.js-token.js-token-${computerColor}-2`).classList.add("game-token-computer-2");
+    document.querySelector(`.js-token.js-token-${computerColor}-3`).classList.add("game-token-computer-3");
+    document.querySelector(`.js-token.js-token-${computerColor}-4`).classList.add("game-token-computer-4");
+    // Update game token positions for user player
+    document.querySelector(`.js-token.js-token-${selectedColor}-1`).classList.add("game-token-user-1");
+    document.querySelector(`.js-token.js-token-${selectedColor}-2`).classList.add("game-token-user-2");
+    document.querySelector(`.js-token.js-token-${selectedColor}-3`).classList.add("game-token-user-3");
+    document.querySelector(`.js-token.js-token-${selectedColor}-4`).classList.add("game-token-user-4");
+  }
+  function updateGameFortStyles(selectedColor = "", computerColor = "", unusedColorsArr = []) {
+    const gameFortEl = document.querySelector(".js-game-fort");
+    const borderTopColor = colorMap[computerColor];
+    const borderRightColor = colorMap[unusedColorsArr[1]];
+    const borderBottomColor = colorMap[selectedColor];
+    const borderLeftColor = colorMap[unusedColorsArr[0]];
+
+    gameFortEl.style.setProperty("--game-fort-br-top", `75px solid ${borderTopColor}`);
+    gameFortEl.style.setProperty("--game-fort-br-right", `75px solid ${borderRightColor}`);
+    gameFortEl.style.setProperty("--game-fort-br-bottom", `75px solid ${borderBottomColor}`);
+    gameFortEl.style.setProperty("--game-fort-br-left", `75px solid ${borderLeftColor}`);
+  }
+  function updateGameHouseStyles(selector, newColor, prevColorClassName) {
+    const el = document.querySelector(selector);
+    const newColorClsName = `bgcolor-${newColor}`;
+
+    el.classList.replace(prevColorClassName, newColorClsName);
+    el.querySelectorAll(".token").forEach(node => {
+      node.classList.replace(prevColorClassName, newColorClsName);
+    });
+  }
+  function updateGameTrackStyles(selector, newColor, prevColorClassName) {
+    const el = document.querySelector(selector);
+    const newColorClsName = `bgcolor-${newColor}`;
+
+    // Update game track colors for computer player
+    el.querySelector(".track-dots").classList.replace(prevColorClassName, newColorClsName);
+  }
+  function setupLayout(selectedColor = "", computerColor = "", unusedColorsArr = []) {
+    // Update game house colors for computer player
+    updateGameHouseStyles(".game-house:nth-of-type(2)", computerColor, "bgcolor-red");
+    // Update game house colors for user player
+    updateGameHouseStyles(".game-house:nth-of-type(3)", selectedColor, "bgcolor-yellow");
+    // Update game house colors for unused top-left player
+    updateGameHouseStyles(".game-house:nth-of-type(1)", unusedColorsArr[0], "bgcolor-blue");
+    // Update game house colors for unused bottom-right player
+    updateGameHouseStyles(".game-house:nth-of-type(4)", unusedColorsArr[1], "bgcolor-green");
+
+    // Update game track colors for computer player
+    updateGameTrackStyles(".game-track.game-track-top", computerColor, "bgcolor-red");
+    // Update game track colors for user player
+    updateGameTrackStyles(".game-track.game-track-bottom", selectedColor, "bgcolor-yellow");
+    // Update game track colors for unused top-left player
+    updateGameTrackStyles(".game-track.game-track-left", unusedColorsArr[0], "bgcolor-blue");
+    // Update game track colors for unused bottom-right player
+    updateGameTrackStyles(".game-track.game-track-right", unusedColorsArr[1], "bgcolor-green");
+
+    // Update game token styles
+    updateGameTokenStyles(selectedColor, computerColor);
+    // Update game fort styles
+    updateGameFortStyles(selectedColor, computerColor, unusedColorsArr);
+  }
   function initGamePlay() {
-    const rivalColorsArr = getRivalColors(selectedColor);
-    const unusedColorsArr = colorSequence.filter(item => rivalColorsArr.indexOf(item) === -1);
+    const activeColorsArr = getActiveColors(selectedColor);
+    const unusedColorsArr = colorSequence.filter(item => activeColorsArr.indexOf(item) === -1);
 
     splashScreenEl.classList.add("d-none");
     appEl.classList.remove("d-none");
     hideUnusedColorTokens(unusedColorsArr);
     hasGameStarted = true;
-    computerColor = rivalColorsArr[1];
+    computerColor = activeColorsArr[1];
+    setupLayout(selectedColor, computerColor, unusedColorsArr);
 
     console.log("user color: ", selectedColor);
     console.log("computer color: ", computerColor);
